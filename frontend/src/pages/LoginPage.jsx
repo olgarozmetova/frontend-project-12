a181d12833ab41853c078a5317d0b999883fe910
olga rozmetova <olga.rozmetova@gmail.com>
2026-01-04 18:59:19 +0100

    create formik form

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Formik, Form, Field } from 'formik'
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Formik
+      initialValues={{
+        username: '',
+        password: '',
+      }}
+      onSubmit={() => {}}
+    >
+      <Form>
+        <div>
+          <label htmlFor="username">Имя пользователя</label>
+          <Field
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Введите имя пользователя"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Пароль</label>
+          <Field
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Введите пароль"
+          />
+        </div>
+
+        <button type="submit">Войти</button>
+      </Form>
+    </Formik>
   )
 }
 

9d08ac942d6f9517a7eb16eaca0a21436cbdb3e1
olga rozmetova <olga.rozmetova@gmail.com>
2026-04-19 19:13:44 +0200

    test rollbar

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { useRollbar } from '@rollbar/react'
 
 const NotFound = () => {
   const { t } = useTranslation()
+  const rollbar = useRollbar()
 
   return (
-    <div className="m-4">
-      <h1>404</h1>
-      <p>{t('notFoundPage.notFound')}</p>
-    </div>
+    <>
+      <div className="m-4">
+        <h1>404</h1>
+        <p>{t('notFoundPage.notFound')}</p>
+      </div>
+
+      <div>
+        <button onClick={() => rollbar.info('Test message from React')}>
+          Send Test Message
+        </button>
+        <button
+          onClick={() => {
+            throw new Error('Test error from React ErrorBoundary')
+          }}
+        >
+          Trigger Test Error
+        </button>
+      </div>
+    </>
   )
 }
 

e911e0a5bd0d0e06511564deb6f01ddeac0aa69c
olga rozmetova <olga.rozmetova@gmail.com>
2026-05-08 00:21:54 +0200

    eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,10 @@
 import js from '@eslint/js'
 import globals from 'globals'
+
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -10,31 +13,37 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
 
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module',
         ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
 
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...js.configs.recommended.rules,
 
-      'arrow-parens': ['error', 'as-needed'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'never'],
-      'brace-style': ['error', '1tbs'],
-      'operator-linebreak': ['error', 'after'],
-      indent: ['error', 2],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      'react-refresh/only-export-components': 'warn',
+
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
 
       'react/react-in-jsx-scope': 'off',
     },

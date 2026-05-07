f06e5aa90c918719bec636527e9b1b5bef5a4284
olga rozmetova <olga.rozmetova@gmail.com>
2026-05-07 22:59:20 +0200

    change eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,20 +2,19 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import stylistic from '@stylistic/eslint-plugin'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    plugins: {
-      '@stylistic': stylistic,
-    },
+
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
